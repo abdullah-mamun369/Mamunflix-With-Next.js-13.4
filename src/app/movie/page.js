b97a2c0f65ce701e33a6be449d94280ccdbe4aac
abdullah-mamun369 <maamamun369@gmail.com>
2023-06-18 03:31:36 +0600

    Data fetch is successfully completed

--- src/app/movie/page.js
+++ src/app/movie/page.js
@@ -5,22 +5,23 @@ import Link from "next/link";
 const Movie = async () => {
 
     const url = process.env.RAPID_API_KEY
+    const RaKey = process.env.X_RapidAPI_Key
+    const RaHost = process.env.X_RapidAPI_Host
 
     const options = {
         method: 'GET',
         headers: {
-            'X-RapidAPI-Key': process.env.X_RapidAPI_Key,
-            'X-RapidAPI-Host': process.env.X_RapidAPI_Host,
+            'X-RapidAPI-Key': RaKey,
+            'X-RapidAPI-Host': RaHost
         }
-
     };
-    console.log('X-RapidAPI-Key', 'X-RapidAPI-Host');
+
 
 
     const res = await fetch(url, options);
     const data = await res.json();
-    const main_data = data.titles;
 
+    console.log(data);
 
     return (
         <>
