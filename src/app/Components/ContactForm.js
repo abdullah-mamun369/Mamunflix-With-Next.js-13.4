e7165c5e9f92e98f4e0859164d829a9ea9423883
abdullah-mamun369 <maamamun369@gmail.com>
2023-06-21 00:50:10 +0600

    Contact testing is worked

--- src/app/Components/ContactForm.js
+++ src/app/Components/ContactForm.js
@@ -18,6 +18,8 @@ const ContactForm = () => {
         message: ""
     })
 
+    const [status, setStatus] = useState(null);
+
 
     const handleChange = (e) => {
         const name = e.target.name;
@@ -26,8 +28,42 @@ const ContactForm = () => {
         setUser((prevUser) => ({ ...prevUser, [name]: value }))
     }
 
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch('../api/contact', {
+                method: 'POST',
+                headers: { "Content_Type": "application/json" },
+                body: JSON.stringify({
+                    username: user.username,
+                    email: user.email,
+                    phone: user.phone,
+                    message: user.message
+                })
+            })
+            // Set the status based on the response from the API route
+            if (response.status === 200) {
+                setUser({
+                    username: "",
+                    email: "",
+                    phone: "",
+                    message: ""
+                })
+                setStatus('success');
+            } else {
+                setStatus('error');
+            }
+
+        } catch (e) {
+            console.log(e)
+        }
+
+    }
+
     return (
-        <form className={styles.contact_form} >
+        <form className={styles.contact_form} onSubmit={handleSubmit} >
             <div className={styles.input_field}>
                 <label htmlFor="username" className={styles.label}>
                     Enter your name
@@ -82,16 +118,14 @@ const ContactForm = () => {
                     />
                 </label>
             </div>
+
+
             <div>
-                <button
-                    type="submit"
-                    className={mulish.className}
-                >
-                    Submit
-                </button>
+                {status === 'success' && <p className={styles.success_msg}>Thank you for your message!</p>}
+                {status === 'error' && <p className={styles.error_msg}>There was an error submitting your message. Please try again.</p>}
+
+                <button type="submit" className={mulish.className}>Send Message</button>
             </div>
-
-
 
         </form>
     );
